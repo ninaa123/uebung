8e7b4a1be7ca39217a6802a7b79295e190b75f23
nina <nina@ninas-mbpro.home>
2014-04-05 09:13:12 +0200

    übung

--- ue_sort.c
+++ ue_sort.c
@@ -23,22 +23,79 @@ int *fill(int *a, int elements)
     return a;
 }
 
-int main(int argc, char** argv) {
+void swap(int *a, int *b)
+{
+    int tmp;
+    tmp=*a;
+    *a=*b;
+    *b=tmp;
+}
+
+int cmp(int a, int b)
+{
+    if(a>b)
+        return 0;
+    else return 1;
+}
+
+int *sort(int *p, int elements)
+{
+    int k;
+    int m;
+    int *res;
     
-    int *p;
-    int i;
+    res=(int *) malloc(elements*sizeof(int));
     
-    int elements=atof(argv[1]);
-    
-    p=(int *) malloc(elements*sizeof(int));
-    
-    if (p==NULL) 
+    if (res==NULL) 
     {
         perror("Nicht genug Speicher vorhanden."); 
         exit(EXIT_FAILURE);                        
     }
     
-    fill(p, 100);
+    res[0]=rand();
+    
+    for(k=1;k<elements;++k)
+    {
+        res[k]=rand();
+        
+        for(m=0;m<k;++m)
+        {
+            if(cmp(res[k],res[k-m])==0)
+            {
+                swap(&res[k],&res[k-m]);
+                k=k-m;
+            }
+        }
+    }
+    
+    return res;
+}
+
+int main(int argc, char** argv) {
+    
+    int *p;
+    int *result;
+    int i;
+    
+    int elements=10;
+    
+    p=(int *) malloc(elements*sizeof(int));
+    result=(int *) malloc(elements*sizeof(int));
+    
+    if (p==NULL) 
+    {
+        perror("Nicht genug Speicher vorhanden. p"); 
+        exit(EXIT_FAILURE);                        
+    }
+    
+    if (result==NULL) 
+    {
+        perror("Nicht genug Speicher vorhanden. result"); 
+        exit(EXIT_FAILURE);                        
+    }
+    
+/*
+    fill(p, elements);
     
     FILE *f;
     f=fopen("sort.txt","w");
@@ -49,8 +106,16 @@ int main(int argc, char** argv) {
     }
     
     fclose(f);
+*/
+    
+    result = sort(p, elements);
+    for(int n=0;n<elements;++n)
+    {
+        printf("%d\n",result[n]);
+    }
     
     free(p);
+    free(result);
 
     return (EXIT_SUCCESS);
 }
